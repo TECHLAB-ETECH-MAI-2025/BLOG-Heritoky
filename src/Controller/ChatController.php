1277fa7e75cfb9fd7fa3ef3f61785bfe28e77fcc
Kilofa <kilofa@Loki.Lokifa>
2025-06-02 04:55:43 -0400

    chat avec polling fait

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
+
 
 #[Route('/chat')]
 class ChatController extends AbstractController
